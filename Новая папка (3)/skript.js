854c395b5aebecec2c7b3243c8fe7f150352d7c8
EvloevIbra <117371837+EvloevIbra@users.noreply.github.com>
2023-06-21 16:02:35 +0300

    nkkb

--- Новая папка (3)/skript.js
+++
@@ -1,9 +0,0 @@
-let links = document.querySelectorAll(".link_division")
-links.forEach(element => {
-    element.addEventListener("click", function () {
-        element.classList.toggle(".activeLink")
-    })
-});
-console.log(links);
-console.log(window.location.pathname);
-// console.log(window.location.pathname.split("/")[1].split(".")[0]);
